75d0fe05db3d439357380218bbdc9b6c2600fc33
DiegoSullon <jdiego.sullon@gmail.com>
2020-11-26 01:58:10 -0500

    CRUD PLatillo and CRUD Order

---
+++ SysRestaurantNorte/Entity/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Order
+    {
+        public int id {set;get;}
+        public int idMesa { set;get;}
+        public int idCliente {set;get;}
+
+        public List<Platillo> platillo;
+        
+    }
+}
