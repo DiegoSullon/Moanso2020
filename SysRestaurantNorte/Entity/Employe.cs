63378550e2e5fea21cc7535182f4db95b8b4eb01
DiegoSullon <jdiego.sullon@gmail.com>
2020-12-03 01:46:03 -0500

    a

--- SysRestaurantNorte/Entity/Employe.cs
+++ SysRestaurantNorte/Entity/Employe.cs
@@ -9,13 +9,13 @@ namespace Entity
     public class Employe
     {
         public int id { get; set; }
-        public string dni { get; set; }
         public string name { get; set; }
+        public int idRol { get; set; }
         public string apellido { get; set; }
+        public string dni { get; set; }
         public string cv { get; set; }
         public string telefono { get; set; }
         public DateTime fNacimiento { get; set; }
-        public int idRol { get; set; }
 
     }
 }
